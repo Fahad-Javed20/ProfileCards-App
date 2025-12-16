7baece61610935521cc4c558fbf63eb3e0602240
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-16 10:07:01 +0500

    complete server in express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,9 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.json({message:"Hello World"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
 
 app.listen(PORT, () => {
-    console.log(`server is listining at port ${PORT}`)});
+  console.log(`server is listining at port ${PORT}`);
+});
